9c7ba3cd6fe8787626460a89c96a5ec165692a10
loayeldin <loayeldin123@gmail.com>
2023-07-04 15:38:57 +0300

    edit order.ts

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent {
-
+constructor(private authService:AuthService){}
+token = this.authService.user.value.token
 }
